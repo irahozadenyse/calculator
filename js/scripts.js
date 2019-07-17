ab24813bcf061c7e0c84ece88192894b1d314bb3
irahozadenyse <irahodeny1@gmail.com>
2019-07-17 08:22:08 +0200

    added javascript code

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+/*var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(add(number1, number2));
+
+  var substraction = function(number1, number2) {
+      return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number: "));
+  var number2 = parseInt(prompt("Ent a number: "));
+  var result = substraction(number1, number2);
+  alert(result);
+
+  var multiplication = function(number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(multiplication(number1, number2));
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(divide(number1, number2));
+
+  var calcBmi = function(weight, height) {
+      return weight / height^2;
+  }
+  var weight = parseInt(prompt("Enter a number:"));
+  var height = parseInt(prompt("Enter another number: "));
+  var result = calcBmi(weight, height);
+  alert(result);
+
+  var temperature = function(Celsius , Fahrenheit) {
+      return Fahrenheit = Celsius* 1.8 + 32 ;
+  }
+  var Celsius = parseInt(prompt("Enter a number: "));
+  var Fahrenheit = parseInt(prompt("Enter another number"));
+  var result = temperature(Celsius, Fahrenheit);
+  alert(result);
+*/
+  var convert = function(gallon ,litres) {
+      return gallon= litres / 3.87 ;
+  }
+  
+  var litres = parseInt(prompt("Enter a number: "));
+  var gallon = parseInt(prompt("Enter other number: "));
+   var result = convert(gallon, litres);
+   alert(result);
